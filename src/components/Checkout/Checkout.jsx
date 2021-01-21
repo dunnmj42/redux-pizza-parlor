661d0ebefe82cc91a89a1cc094e266c4735d75d6
Michael Dunn <dunnmj42@gmail.com>
2021-01-21 15:54:24 -0600

    checkout header

---
+++ src/components/Checkout/Checkout.jsx
@@ -0,0 +1,69 @@
+import {useDispatch, useSelector} from 'react-redux'
+
+function Checkout() {
+
+  const cart = useSelector(store => store.cartReducer);
+  const customer = useSelector(store => store.customerReducer);
+
+  const dispatch = useDispatch();
+
+  const handleCheckout = (cart, customer) => {
+
+    let customerToSend = {
+      customer_name: customer.name,
+      street_address: customer.address,
+	    city: customer.city,
+	    zip: customer.zip,
+	    type: customer.type,
+      total: cart.total,
+      pizzas: cart.pizzas
+    }
+
+    axios.post('/api/order', customerToSend)
+      .then(response => {
+          const action = {
+            type: "CLEAR",
+            payload: [],
+          };
+
+          dispatch(action);
+          history.push('/');
+      }).catch(err => {
+        console.error(err)
+      })
+
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>Prime Pizza</h1>
+      </header>
+      <h2>Step 3: Checkout</h2>
+      <p>{customer.name}</p>
+      <p>{customer.address}</p>
+      <p>{customer.city}, MN {customer.zip}</p>
+      <p>{customer.type}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((pizza, i) =>
+            <tr key={i}>
+              <td>{pizza.name}</td>
+              <td>{pizza.price}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <h2>Total: {cart.total}</h2>
+      <button onClick={handleCheckout}>Checkout</button>
+    </div>
+  )
+}
+
+export default Checkout;
