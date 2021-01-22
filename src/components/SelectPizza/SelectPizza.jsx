5edb703018ae536ba877e336de702ac15f1bf8a1
collinradichel <radichel.collin@gmail.com>
2021-01-21 21:07:20 -0600

    Cleaned up some code, and added a bit of styling.  Pretty sure I have a plan for the price in the Header & Checkout Component

--- src/components/SelectPizza/SelectPizza.jsx
+++ src/components/SelectPizza/SelectPizza.jsx
@@ -1,31 +1,19 @@
-import {useState} from 'react';
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import PizzaItem from "../PizzaItem/PizzaItem";
 import Button from "react-bootstrap/Button";
-import Header from "../Header/Header"
+import Header from "../Header/Header";
+
 
 function SelectPizza() {
+
   const history = useHistory();
   const dispatch = useDispatch();
 
   const pizzas = useSelector((store) => store.pizzaReducer);
+  const pizzasInCart = useSelector((store) => store.currentCartReducer)
 
-  const [isItemSelected, setIsItemSelected] = useState(false);
-
-  const [pizzasInCart, setPizzasInCart] = useState([]);
-
-  const toggleSelect = (pizza) => {
-    console.log(`isItemSelected is:`, !isItemSelected);
-    setIsItemSelected(!isItemSelected);
-    if (isItemSelected) {
-      setPizzasInCart([...pizzasInCart, pizza]);
-    } else if (!isItemSelected) {
-      const matchPizza = (pizza) => pizza.id !== pizza.id;
-      setPizzasInCart(pizzasInCart.filter(matchPizza));
-    }
-  };
 
   const proceedToForm = (pizzasInCart) => {
     console.log(`Moving to customer info page`);
@@ -35,16 +23,18 @@ function SelectPizza() {
 
   return (
     <section>
+
       <Header />
       <h2>Step 1: Select Your Pizza</h2>
 
       {pizzas.map((pizza, i) => (
-        <PizzaItem key={i} pizza={pizza} toggleSelect={toggleSelect}/>
+        <PizzaItem key={i} pizza={pizza}/>
       ))}
-      
+
       <div className="proceedToForm">
-        <Button onClick={() => proceedToForm(pizzasInCart)}></Button>
+        <Button variant="success" size="lg" block onClick={() => proceedToForm(pizzasInCart)}>Next Step</Button>
       </div>
+
     </section>
   );
 }
